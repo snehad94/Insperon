9779bae3f288e98a31c2c23fbfbf0d842c367669
Sneha D <snehad94@gmail.com>
2019-01-22 11:43:37 +0530

    Update app.js

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,16 +1,7 @@
 /*Author : Sneha D
   Purpose : For Angular Module Creation
   Created Date : 10/01/2019 */
-
-
 var app = angular.module('App', [
   'App.controllers',
   'App.services'
 ]);
-
-
-// const PORT = process.env.PORT || 8080;
-
-// app.listen(PORT);
-
-// console.log(PORT);
